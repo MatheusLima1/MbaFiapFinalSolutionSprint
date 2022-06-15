06bc01b24875458a274ebc01f5a12199159b2781
Matheus Lima <matheusinclima@gmail.com>
2022-06-14 22:06:46 -0300

    creating features

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.myapplication.ui.main.MainFragment
+import com.example.myapplication.ui.password.PasswordFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,18 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+    }
+
+    companion object {
+        fun goToFragment(
+            fragmentManager: FragmentManager?,
+            fragment: Fragment,
+            bundle: Bundle? = null
+        ) {
+            fragmentManager?.beginTransaction()
+                ?.replace(R.id.container, fragment)
+                ?.commitNow()
         }
     }
 }
--- app/src/main/java/com/example/myapplication/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/main/MainFragment.kt
@@ -1,32 +1,68 @@
 package com.example.myapplication.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.MainActivity
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentMainBinding
+import com.example.myapplication.ui.password.PasswordFragment
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
+    lateinit var binding: FragmentMainBinding
+    private var isChecked = false
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding =
+            FragmentMainBinding.bind(inflater.inflate(R.layout.fragment_main, container, false))
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
+    override fun onResume() {
+        super.onResume()
+        binding.recaptchaLayout.chbEnableCaptcha.setOnCheckedChangeListener { compoundButton, isChecked ->
+            this.isChecked = isChecked
+            checkCanGoThrough(isChecked)
+        }
+        binding.edtCnpjCpf.addTextChangedListener (object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                checkCanGoThrough(isChecked)
+            }
+        })
+        binding.btnSignIn.setOnClickListener {
+            MainActivity.goToFragment(fragmentManager, PasswordFragment.newInstance())
+        }
+    }
+
+    private fun checkCanGoThrough(isChecked: Boolean) {
+        if (isChecked) {
+            binding.btnSignIn.isEnabled = binding.edtCnpjCpf.text.length >= 11
+        } else {
+            binding.btnSignIn.isEnabled = false
+        }
+    }
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
 }
